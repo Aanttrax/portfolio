bc156d2364ead55fe6ef7f0bf4729348d3eae7d6
Aanttrax <arielrt20@gmail.com>
2024-11-01 21:39:35 -0400

    feat: :sparkles: se crean los compoonentes con la estructura

---
+++ src/app/components/spaceman/spaceman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceman',
+  standalone: true,
+  imports: [],
+  templateUrl: './spaceman.component.html',
+  styleUrl: './spaceman.component.scss',
+})
+export class SpacemanComponent {}
