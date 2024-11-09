6c12b57187d779382a2cc63e871f4c2e0ab5a07f
Aanttrax <arielrt20@gmail.com>
2024-11-09 09:31:16 -0500

    feat: :sparkles: creacion de cardExperience

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '@components/hero/hero.component';
 import { NavbarComponent } from '@shared/navbar/navbar.component';
 import { PortfolioComponent } from '@components/portfolio/portfolio.component';
+import { ExperienceComponent } from '@components/experience/experience.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent, PortfolioComponent],
+  imports: [HeroComponent, NavbarComponent, PortfolioComponent, ExperienceComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
