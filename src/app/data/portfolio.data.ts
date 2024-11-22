5cfeaf3c608481a86c89c2121457b6b9095b5d01
Aanttrax <arielrt20@gmail.com>
2024-11-22 07:08:30 -0500

    feat: :sparkles: implementacion url github and linkedin

--- src/app/data/portfolio.data.ts
+++ src/app/data/portfolio.data.ts
@@ -9,11 +9,13 @@ export const portfolio = [
     description:
       'This is a portfolio project built with Angular 18. It incorporates the 3D library "Three.js" and leverages the latest features of Angular. Additionally, the project implements CI/CD pipelines using GitHub Actions, incorporating tools like Husky and lint-staged to enforce code quality through pre-commit hooks and staged file linting',
     image: images['portfoliov1'],
+    link: 'https://aanttrax.github.io/portfolio/',
   },
   {
     name: 'Rick and Morty Angular App',
     description:
       'This is a sample project that uses Angular 16 to display information about the "Rick and Morty" TV series. The project utilizes the public [Rick and Morty API](https://rickandmortyapi.com/).',
     image: images['rickMorty'],
+    link: 'https://bright-frangollo-70b51c.netlify.app/characters-list',
   },
 ];
